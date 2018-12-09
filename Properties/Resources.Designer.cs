39d6b4437f10580cdfbef0cde682de01c6ee391a
psieg <github@psieg.de>
2018-12-09 16:53:20 +0100

    Add embedded font for UDisplay

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace Judoplaner2.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] MachineStd {
+            get {
+                object obj = ResourceManager.GetObject("MachineStd", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
     }
 }
--- UDisplay.cs
+++ UDisplay.cs
@@ -1,12 +1,34 @@
 using System;
 using System.Windows.Forms;
+using System.Drawing.Text;
+
 namespace UDisplay
 {
     public partial class TDisplayForm: Form
     {
+        [System.Runtime.InteropServices.DllImport("gdi32.dll")]
+        private static extern IntPtr AddFontMemResourceEx(IntPtr pbFont, uint cbFont,
+            IntPtr pdv, [System.Runtime.InteropServices.In] ref uint pcFonts);
+
+        private PrivateFontCollection fonts = new PrivateFontCollection();
+
+        System.Drawing.Font myFont;
+        System.Drawing.FontFamily myFontFamily;
+
         public TDisplayForm()
         {
             InitializeComponent();
+
+            byte[] fontData = Judoplaner2.Properties.Resources.MachineStd;
+            IntPtr fontPtr = System.Runtime.InteropServices.Marshal.AllocCoTaskMem(fontData.Length);
+            System.Runtime.InteropServices.Marshal.Copy(fontData, 0, fontPtr, fontData.Length);
+            uint dummy = 0;
+            fonts.AddMemoryFont(fontPtr, Judoplaner2.Properties.Resources.MachineStd.Length);
+            AddFontMemResourceEx(fontPtr, (uint)Judoplaner2.Properties.Resources.MachineStd.Length, IntPtr.Zero, ref dummy);
+            System.Runtime.InteropServices.Marshal.FreeCoTaskMem(fontPtr);
+
+            myFontFamily = fonts.Families[0];
+            myFont = new System.Drawing.Font(fonts.Families[0], 16.0F);
         }
 
         public void FormResize(System.Object Sender, System.EventArgs _e1)
@@ -17,9 +39,9 @@ namespace UDisplay
                 this.Font.Style, this.Font.Unit);
             var PlayersFont = new System.Drawing.Font(this.PlayerALabel.Font.Name, (int)Math.Round(this.Height / 40.0),
                 this.Font.Style, this.Font.Unit);
-            var CaptionFont = new System.Drawing.Font(this.PlayerA10Caption.Font.Name, (int)Math.Round(this.Height / 30.0),
+            var CaptionFont = new System.Drawing.Font(myFontFamily, (int)Math.Round(this.Height / 30.0),
                 this.Font.Style, this.Font.Unit);
-            var NumbersFont = new System.Drawing.Font(this.PlayerA10Label.Font.Name, (int)Math.Round(this.Height / 20.0),
+            var NumbersFont = new System.Drawing.Font(myFontFamily, (int)Math.Round(this.Height / 20.0),
                 this.Font.Style, this.Font.Unit);
             var BottomFont = new System.Drawing.Font(this.PrepareLabel.Font.Name, (int)Math.Round(this.Height / 55.0),
                 this.Font.Style, this.Font.Unit);
